1305c0fdf907f615c88c160bbccbb06f647d6fd0
uo271288 <uo271288@uniovi.es>
2024-04-14 12:44:54 +0200

    First steps with PWA It's a demo Added manifest, service worker, sw registration and a button.

---
+++ src/components/Layout.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+function Layout(props){
+    
+const [isReadyForInstall, setIsReadyForInstall] = useState(false);
+
+useEffect(() => {
+  window.addEventListener("beforeinstallprompt", (event) => {
+    // Prevent the mini-infobar from appearing on mobile.
+    event.preventDefault();
+    console.log("👍", "beforeinstallprompt", event);
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Remove the 'hidden' class from the install button container.
+    setIsReadyForInstall(true);
+  });
+}, [])
+
+async function downloadApp() {
+  console.log("👍", "butInstall-clicked");
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    // The deferred prompt isn't available.
+    console.log("oops, no prompt event guardado en window");
+    return;
+  }
+  // Show the install prompt.
+  promptEvent.prompt();
+  // Log the result
+  const result = await promptEvent.userChoice;
+  console.log("👍", "userChoice", result);
+  // Reset the deferred prompt variable, since
+  // prompt() can only be called once.
+  window.deferredPrompt = null;
+  // Hide the install button.
+  setIsReadyForInstall(false);
+}
+
+return(<>{isReadyForInstall && <button onClick={downloadApp}>Descarga</button>}</>)
+}
+
+export default Layout
