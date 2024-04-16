4ca46b8401fd52b82e5ed27904046237805a32d3
uo271288 <uo271288@uniovi.es>
2024-04-16 21:13:09 +0200

    Added locales Removed unused files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg'
-import './App.css'
-import Layout from './components/Layout';
+import logo from './logo.png';
+import { useEffect, useState } from "react";
+import { useTranslation } from 'react-i18next';
 
 function App() {
+
+  const locales = {
+    es: { title: 'Español' },
+    en: { title: 'English' }
+  };
+
+  const { t, i18n } = useTranslation();
+
+  const [isReadyForInstall, setIsReadyForInstall] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (event) => {
+      event.preventDefault();
+      window.deferredPrompt = event;
+      setIsReadyForInstall(true);
+    });
+  }, []);
+
+  async function downloadApp() {
+    const promptEvent = window.deferredPrompt;
+    if (promptEvent) {
+      promptEvent.prompt();
+      await promptEvent.userChoice;
+      window.deferredPrompt = null;
+      setIsReadyForInstall(false);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Layout/>
+        <img src={logo} alt="logo" />
+        <ul>
+          {Object.keys(locales).map((locale) => (
+            <li key={locale}><button style={{ fontWeight: i18n.resolvedLanguage === locale ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(locale)}>
+              {locales[locale].title}
+            </button></li>
+          ))}
+        </ul>
+        <h1>{t('welcome-message')}</h1>
+        {isReadyForInstall && <button onClick={downloadApp}>Descarga</button>}
       </header>
     </div>
   );
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,28 +1,26 @@
 /* eslint-disable no-restricted-globals */
-import {clientsClaim} from "workbox-core"
-import {ExpirationPlugin} from "workbox-expiration"
-import {precacheAndRoute, createHandlerBoundToURL} from "workbox-precaching"
-import {registerRoute} from "workbox-routing"
-import {StaleWhileRevalidate} from "workbox-strategies"
+import { clientsClaim } from "workbox-core";
+import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
 
-clientsClaim()
-precacheAndRoute(self.__WB_MANIFEST)
+clientsClaim();
+precacheAndRoute(self.__WB_MANIFEST);
 
-const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$")
+const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
-  ({request, url}) => {
+  ({ request, url }) => {
     // If this isn't a navigation, skip.
     if (request.mode !== "navigate") {
-      return false
+      return false;
     } // If this is a URL that starts with /\_, skip.
     if (url.pathname.startsWith("/_")) {
-      return false
+      return false;
     } // If this looks like a URL for a resource, because it contains // a file extension, skip.
     if (url.pathname.match(fileExtensionRegexp)) {
-      return false
+      return false;
     } // Return true to signal that we want to use the handler.
-    return true
+    return true;
   },
   createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
-)
+);
